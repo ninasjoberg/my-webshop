746f5a64fdefa5795360955e14ec56e07e88997e
ninasjoberg <nina.sjoberg@gmail.com>
2019-02-02 21:42:46 +0100

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+import Layout from '../components/MyLayout.js'
+
+
+export default () => (
+	<div>
+		<Header />
+		<Layout>
+			<p>This is the about page</p>
+		</Layout>
+	</div>
+)
---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import Header from '../components/Header'
+import Layout from '../components/MyLayout.js'
+
+
+const PostLink = (props) => (
+	<li>
+		<Link
+			as={`/p/${props.id}`}
+			href={`/post?title=${props.title}`}
+		>
+			<a>{props.title}</a>
+		</Link>
+	</li>
+)
+
+
+const Index = () => (
+	<div>
+		<Header />
+		<Layout>
+			<h1>My Blog</h1>
+			<ul>
+				<PostLink id="hello-nextjs" title="Hello Next.js" />
+				<PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+				<PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+			</ul>
+		</Layout>
+	</div>
+)
+
+export default Index
