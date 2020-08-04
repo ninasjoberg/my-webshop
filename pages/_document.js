9e2b3f5150f0dac4bd42789b0528aa7b5c12cec8
ninasjoberg <nina.sjoberg@gmail.com>
2020-08-04 22:58:51 +0200

    add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,9 @@ export default class MyDocument extends Document {
             <Head>
                 <title>bell pepper: silversmycken</title>
                 <meta property="og:title" content="bell pepper: silversmycken"></meta>
+                <meta property="og:type" content="website"></meta>
                 <meta property="og:description" content="Handgjorda smycken i 925 sterling silver, tillverkade i liten skala. Kvinnosymboler, geometriska former och stilren design."></meta>
+                <meta name="description" content="Handgjorda smycken i 925 sterling silver, tillverkade i liten skala. Kvinnosymboler, geometriska former och stilren design."></meta>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
                 <meta name="google-site-verification" content="P62ChL8wEUwp2QCClCPZzZ5Apk4xZm1sIfI9T-z0fsE" />
